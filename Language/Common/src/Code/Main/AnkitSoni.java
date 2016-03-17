e4bbbbe1e9fcf014926df68e0ce7c578c57fb399
Rajpurohit <ashok1113@gmail.com>
2016-03-17 18:45:37 +0530

    Ankit Soni's prime number generate algorithm implementation.

--- Language/Common/src/Code/Main/AnkitSoni.java
+++ Language/Common/src/Code/Main/AnkitSoni.java
@@ -5,6 +5,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Problem Name: Ankit Soni
  * Link: Office
@@ -31,6 +35,59 @@ public class AnkitSoni {
             int n = in.readInt(), k = in.readInt();
             out.println(chipMonkOne(n, k));
         }
+    }
+
+    public static int[] gen_prime(int n) {
+        int[] primes;
+        int sqrt = (int)Math.sqrt(n);
+        LinkedList<Integer> prime = new LinkedList<Integer>();
+
+        for (int i = 6; i <= n; i += 6) {
+            int root = (int)Math.sqrt(i);
+            Iterator<Integer> iter = prime.iterator();
+            boolean isPrime = true;
+            int v = i - 1;
+
+            while (iter.hasNext() && isPrime) {
+                int num = iter.next();
+                if (num > root)
+                    break;
+
+                isPrime = v % num != 0;
+            }
+
+            if (isPrime)
+                prime.add(v);
+
+            isPrime = true;
+            v = i + 1;
+
+            iter = prime.iterator();
+
+            while (iter.hasNext() && isPrime) {
+                int num = iter.next();
+                if (num > root + 1)
+                    break;
+
+                isPrime = v % num != 0;
+            }
+
+            if (isPrime)
+                prime.add(v);
+        }
+
+        primes = new int[prime.size() + 2];
+        primes[0] = 2;
+        primes[1] = 3;
+
+        int index = 2;
+        Iterator<Integer> iter = prime.iterator();
+
+        while (iter.hasNext()) {
+            primes[index++] = iter.next();
+        }
+
+        return primes;
     }
 
     private static long chipMonkOne(int n, int k) {
